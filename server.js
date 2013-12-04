c08f893858c21d32409a0df9dcee1bfc83a46d53
Martin Komischke <martin@6wunderkinder.com>
2013-12-04 08:55:00 +0100

    * refined server.js (some more eventhandlers)

--- server.js
+++ server.js
@@ -4,13 +4,29 @@ var WebSocketServer = require('ws').Server
   , wss = new WebSocketServer({port: 8080, host:"127.0.0.1"});
   
 wss.on('connection', function(ws) {
-	var rs = fs.createReadStream("response.json", { 'flags': 'r',
-		'encoding': 'binary',
-		'mode': 0666,
-		'bufferSize':64 * 1024
-	});
+	console.log('new connection');
 	
-	rs.on('data', function(data) {
-		ws.send(data);
-    });
+	ws.on('message', function(data, flags) {
+		console.log('message received');
+
+		var rs = fs.createReadStream("response.json", { 'flags': 'r',
+			'encoding': 'binary',
+			'mode': 0666,
+			'bufferSize':64 * 1024
+		});
+		
+		rs.on('data', function(data) {
+			console.log('sending data');
+			ws.send(data);
+		});
+		
+	  });
+	  
+	ws.on('close', function() {
+		console.log('close');
+	  });
+
+	  ws.on('error', function(e) {
+		console.log('error occured %s', e.message);
+	  }); 	
 });
